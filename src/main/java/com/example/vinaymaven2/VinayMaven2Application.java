03ee601de31b3fc41c6669325cd9e4e76c37b340
Vinay Kumar P <p.vinaypkv@gmail.com>
2023-05-06 10:48:00 +0530

    first commit

---
+++ src/main/java/com/example/vinaymaven2/VinayMaven2Application.java
@@ -0,0 +1,13 @@
+package com.example.vinaymaven2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinayMaven2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinayMaven2Application.class, args);
+	}
+
+}
